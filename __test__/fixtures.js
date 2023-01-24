485d1ae579b4fca018b3d7d65f4c0d6b774fcdbe
Bruno Peixoto <brunolnetto@gmail.com>
2023-01-23 21:10:28 -0300

    lint/ make lint

--- __test__/fixtures.js
+++ __test__/fixtures.js
@@ -1,21 +1,24 @@
-import _ from 'lodash';
-import { buildAssertion, buildScenario } from '../index';
+import _ from "lodash";
+import { buildAssertion, buildScenario } from "../index";
 
-const expectToBeDefined = (result) => expect(result).toBeDefined()
-const expectToBe = (result, expected) => expect(result).toBe(expected)
+const expectToBeDefined = (result) => expect(result).toBeDefined();
+const expectToBe = (result, expected) => expect(result).toBe(expected);
 
 /*
  * assert fixtures
  */
-export const assertFixtures = [ [ 42, expectToBeDefined ], [ '42', '42', expectToBe ] ];
+export const assertFixtures = [
+  [42, expectToBeDefined],
+  ["42", "42", expectToBe],
+];
 
 // Valid samples
 export const validAssertLength1Item = assertFixtures[0];
 export const validAssertLength2Item = assertFixtures[1];
 
 // Error-prone samples
-export const invalidAssertItemLength = ['42'];
-export const invalidAssertCallbackItem = ['42', '42'];
+export const invalidAssertItemLength = ["42"];
+export const invalidAssertCallbackItem = ["42", "42"];
 
 /*
  * atest fixtures
@@ -23,9 +26,9 @@ export const invalidAssertCallbackItem = ['42', '42'];
 export let validAtestScenario;
 let setup, prepare, exercise, teardown;
 
-export let validAtestFixture = '42';
+export let validAtestFixture = "42";
 const resultFunction = (fixture_) => fixture_;
-let expectedAtestResult = '42';
+let expectedAtestResult = "42";
 
 setup = () => {};
 
--- __test__/jestus.test.js
+++ __test__/jestus.test.js
@@ -1,4 +1,4 @@
-import { assert, batchAssert, atest, batchAtest } from '../index';
+import { assert, batchAssert, atest, batchAtest } from "../index";
 
 import {
   assertFixtures,
@@ -8,45 +8,45 @@ import {
   invalidAssertCallbackItem,
   validAtestFixture,
   validAtestScenario,
-} from './fixtures';
+} from "./fixtures";
 
 let fixtures, scenarios;
 
-describe('assert', () => {
-  it('should assert on result-callback pattern', () => {
+describe("assert", () => {
+  it("should assert on result-callback pattern", () => {
     expect.assertions(1);
     assert(validAssertLength1Item);
   });
 
-  it('should assert on result-expected-callback pattern', () => {
+  it("should assert on result-expected-callback pattern", () => {
     expect.assertions(1);
     assert(validAssertLength2Item);
   });
 
-  it('should throw error on item with length different than 2 or 3', () => {
+  it("should throw error on item with length different than 2 or 3", () => {
     const invalidArgumentFunction = () => assert(invalidAssertItemLength);
     expect(invalidArgumentFunction).toThrow(Error);
   });
 
-  it('should throw error on invalid callback function', () => {
+  it("should throw error on invalid callback function", () => {
     const invalidCallbackFunction = () => assert(invalidAssertCallbackItem);
     expect(invalidCallbackFunction).toThrow(Error);
   });
 });
 
-describe('batchAssert', () => {
-  it('should assert asserts in batch', () => {
+describe("batchAssert", () => {
+  it("should assert asserts in batch", () => {
     expect.assertions(assertFixtures.length);
     batchAssert(assertFixtures);
   });
 });
 
-describe('atest', () => {
-  it('should assert atest', () => atest(validAtestFixture, validAtestScenario));
+describe("atest", () => {
+  it("should assert atest", () => atest(validAtestFixture, validAtestScenario));
 });
 
-describe('batchAtest', () => {
-  it('should assert batchAtest', () => {
+describe("batchAtest", () => {
+  it("should assert batchAtest", () => {
     fixtures = [validAtestFixture, validAtestFixture];
     scenarios = [validAtestScenario, validAtestScenario];
 

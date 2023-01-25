be66cc32fa5bbadcb0eac21b56f55e67f79df028
Bruno Peixoto <brunolnetto@gmail.com>
2023-01-24 21:08:55 -0300

    feat/ remove stupid node_modules and coverage

---
+++ examples/node_modules/jestus/examples/module/__test__/operations.test.js
@@ -0,0 +1,24 @@
+import { sketch, prove } from "../../../jestus";
+
+import {
+  addTestCases,
+  multiplyTestCases,
+  subtractTestCases,
+  divisionTestCases,
+} from "./testCases";
+
+/*
+ *  Test design
+ */
+
+let sketches;
+
+/** Description **/
+sketches = [
+  ["add", () => sketch(addTestCases)],
+  ["multiply", () => sketch(multiplyTestCases)],
+  ["subtract", () => sketch(subtractTestCases)],
+  ["divide", () => sketch(divisionTestCases)],
+];
+
+prove(sketches);
